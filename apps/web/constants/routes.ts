c56125f267ff16c92f7b2d360a015bdec24cbb01
= <=>
2026-07-16 02:02:49 +0530

    updates

---
+++ apps/web/constants/routes.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  HOME: "/",
+  PRODUCTS: "/products",
+  DEALER: "/dealer",
+  CONTACT: "/contact",
+  ABOUT: "/about",
+}
---
+++ apps/web/constants/site.ts
@@ -0,0 +1,5 @@
+export const SITE = {
+  name: "Rustar Chem",
+  url: "https://rustarchem.com",
+  description: "Premium Car & Bike Care Products",
+}
