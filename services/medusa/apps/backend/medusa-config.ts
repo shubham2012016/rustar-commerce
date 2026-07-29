e5184234c908c1cdb55d7336acbb86d45b2cf67b
= <=>
2026-07-30 01:15:16 +0530

    Disable embedded admin for production

--- services/medusa/apps/backend/medusa-config.ts
+++ services/medusa/apps/backend/medusa-config.ts
@@ -5,12 +5,18 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd())
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
+
       jwtSecret: process.env.JWT_SECRET,
       cookieSecret: process.env.COOKIE_SECRET,
     },
+  },
+
+  admin: {
+    disable: true,
   },
 })
